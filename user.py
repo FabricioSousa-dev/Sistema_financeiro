8a423799da95495e5aa9f5a85851651e98e32147
FabricioSousa-dev <ffabricioteixeira15@gmail.com>
2026-03-25 16:55:51 -0300

    first commit

---
+++ user.py
@@ -0,0 +1,5 @@
+username = str(input("Enter your username : ")).split()
+id = str(input("Enter your id: ")).split()
+name = str(input("Enter your name: ")).split()
+
+print("Hello! {}".format(username))
